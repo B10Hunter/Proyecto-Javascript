5dd4cfa6ab9d374b378fa46e26d2a666ac209271
Lucas Albornoz <flipskate@outlook.es>
2022-07-25 13:40:36 -0300

    Proyecto

---
+++ js/index.js
@@ -0,0 +1,57 @@
+class User {
+    constructor (username,email,password){
+        this.username = username
+        this.email = email
+        this.password = password
+        
+    }
+    
+}
+
+let persona  = []
+
+const botonAlert = document.getElementById('idAlerta')    
+const form = document.getElementById('idform')
+
+form.addEventListener('submit', (event) => {
+       event.preventDefault()
+      
+       let username = document.getElementById('idNombre').value
+       let email = document.getElementById('idEmail').value
+       let password = document.getElementById ('idContra').value
+
+       const user = new User (username, email ,password)
+       
+    
+       persona.push (user)
+       console.log(persona)
+       
+   if (localStorage.getItem('persona')) {
+        persona = JSON.parse(localStorage.getItem('persona'))
+    } else {
+        localStorage.setItem('persona', JSON.stringify(persona))
+    }
+  
+})
+botonAlert.addEventListener('click', () => {
+    let arraytStorage = JSON.parse(localStorage.getItem('persona'))
+
+    arraytStorage.forEach((persona, indice) => {
+        form.innerHTML += `<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Tu usario es</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <p class="tamañoTex text-dark"> Tu nombre es : ${persona.username}<br><br>
+               Tu Email es : ${persona.email} </p>
+            </div>
+          </div>
+        </div>
+      </div>
+        `
+         
+    });
+})
